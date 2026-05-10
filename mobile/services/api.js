3f8b451f7e4e9948f2ee20569ce6ed3c515cfae6
Mehmet Emin Yılmaz <m.eminylmz2303@gmail.com>
2026-05-10 14:19:49 +0300

    fix: mobil api isteklerinde AsyncStorage import hatası giderildi

--- mobile/services/api.js
+++ mobile/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-
 import Constants from 'expo-constants';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const getBackendIP = () => {
   const debuggerHost = Constants.expoConfig?.hostUri;
